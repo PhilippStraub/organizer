4e27d9deef2363026aa752458371368814451231
Paedde <github@jpstr.de>
2020-05-01 19:23:39 +0200

    Homescreen ist jetzt cool

--- src/home.js
+++ src/home.js
@@ -25,25 +25,27 @@ class Home extends React.Component{
     //that way we're able to include them here in our home view but also seperate them.
     render() {
         return(
-            <div>
-                
-                <div className="content" id="dozenten">
+            <div><br></br><br></br>
+                <div className="rahmenlogohome"></div>
+              <div id="begruessung">
+                 <div className="content" id="begruessung2">
                     <a>
-                        <h1 className="display-4">Dozenten</h1>
+                        <h1 className="display-4">Herzlich Willkommen {getCookie("user")}!</h1>
+
                         <hr></hr>
-                    </a>
-                
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        </a>
+                        <div className="beg-text">
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        </div>    <iframe className="youtubevideo"src="https://www.youtube.com/embed/tgbNymZ7vqY">
+</iframe>
+
+                    
+                   
+                    
                             
                 </div>
-                <div className="content" id="termine">
-                    <a>
-                        <h1 className="display-4">Termine</h1>
-                        <hr></hr>
-                    </a>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                            
-                </div>
+                
+            </div> 
             </div>
 
         );
